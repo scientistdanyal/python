3ee9c125e3339f54dcc494db4ddd6975f64b1570
yalgar0 <127227129+yalgar0@users.noreply.github.com>
2023-06-26 18:32:31 +0000

    Add while loop for user control

--- rock_paper game/mian.py
+++ rock_paper game/mian.py
@@ -5,24 +5,31 @@ import random
 #options for system
 options = ('rock','papar','scissor')
 
-# user input space
-player = input("Enter your choice(rock, papar and scissor: )").lower()
+
+running = True
+
+while running:
+    # user input space
+    player = input("Enter your choice(rock, papar and scissor: )").lower()   
+    #generate random option for system
+    player_sys = random.choice(options)
+
+    print(f'User: {player}')
+    print(f'System: {player_sys}')
+
+    # statmenets
+    if player == player_sys:
+       print('Match tie')
+    elif player == 'papar' and player_sys == 'rock':
+       print('You won')
+    elif player == 'rock' and player_sys == 'scissor ':
+       print('You won')
+    elif player == 'scissor' and player_sys == 'paper':
+        print('You won') 
+    else:
+        print('you lose')
+    if not input("Play again? (y/n): ").lower() == "y":
+        running = False
 
 
-#generate random option for system
-player_sys = random.choice(options)
-
-print(f'User: {player}')
-print(f'System: {player_sys}')
-
-# statmenets
-if player == player_sys:
-    print('Match tie')
-elif player == 'papar' and player_sys == 'rock':
-    print('You won')
-elif player == 'rock' and player_sys == 'scissor ':
-    print('You won')
-elif player == 'scissor' and player_sys == 'paper':
-    print('You won') 
-else:
-    print('you lose')
+print('Thanks for playing')    
