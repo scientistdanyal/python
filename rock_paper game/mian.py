6831b034b0c2b34c0251ac065ff347c8eb58fa61
yalgar0 <127227129+yalgar0@users.noreply.github.com>
2023-06-26 17:54:42 +0000

    code commenting are added

--- rock_paper game/mian.py
+++ rock_paper game/mian.py
@@ -1,17 +1,21 @@
+# add buit in module of random
 import random
 
 
-
+#options for system
 options = ('rock','papar','scissor')
 
-player = input("Enter your choice(rock, papar and scissor)").lower()
+# user input space
+player = input("Enter your choice(rock, papar and scissor: )").lower()
 
+
+#generate random option for system
 player_sys = random.choice(options)
 
 print(f'User: {player}')
 print(f'System: {player_sys}')
 
-
+# statmenets
 if player == player_sys:
     print('Match tie')
 elif player == 'papar' and player_sys == 'rock':
