8cccaf5964017d6c22b214fb310e64a2486e0257
yalgar0 <127227129+yalgar0@users.noreply.github.com>
2023-06-26 17:50:41 +0000

    commit

--- rock_paper game/mian.py
+++ rock_paper game/mian.py
@@ -2,11 +2,23 @@ import random
 
 
 
-options = ('rock','paper','scissor')
+options = ('rock','papar','scissor')
 
-player = input("Enter your choice(rock, paper and scissor)").lower()
+player = input("Enter your choice(rock, papar and scissor)").lower()
 
 player_sys = random.choice(options)
 
+print(f'User: {player}')
+print(f'System: {player_sys}')
 
-print(player_sys)
+
+if player == player_sys:
+    print('Match tie')
+elif player == 'papar' and player_sys == 'rock':
+    print('You won')
+elif player == 'rock' and player_sys == 'scissor':
+    print('You won')
+elif player == 'scissor' and player_sys == 'paper':
+    print('You won') 
+else:
+    print('you lose')
