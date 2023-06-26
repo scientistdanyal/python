c0cc8297d59f9e2d4c797070e1c1a5524bb5d380
yalgar0 <127227129+yalgar0@users.noreply.github.com>
2023-06-26 17:41:08 +0000

    commit

--- rock_paper game/mian.py
+++ rock_paper game/mian.py
@@ -1,1 +1,12 @@
 import random
+
+
+
+options = ('rock','paper','scissor')
+
+player = input("Enter your choice(rock, paper and scissor)").lower()
+
+player_sys = random.choice(options)
+
+
+print(player_sys)
