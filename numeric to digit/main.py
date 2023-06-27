de388a231f24b42889dd091ec57c5cc2274a05e0
yalgar0 <127227129+yalgar0@users.noreply.github.com>
2023-06-27 10:10:35 +0000

    first commit

---
+++ numeric to digit/main.py
@@ -0,0 +1,17 @@
+def numeric_to_word(num):
+    ones = [
+        'one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'
+    ]
+    
+
+    tens
+
+
+
+
+
+
+
+num = int(input('Enter a number: '))
+
+print(numeric_to_word(num))
