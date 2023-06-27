00a35a4450d8203951b5c56ff592e0f75d8aa4ac
yalgar0 <127227129+yalgar0@users.noreply.github.com>
2023-06-27 10:38:50 +0000

    enthusiastic to complete this project

--- numeric to digit/main.py
+++ numeric to digit/main.py
@@ -1,17 +1,47 @@
-def numeric_to_word(num):
+def number_to_words(num):
     ones = [
-        'one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'
     ]
+
+    tens = [
+        '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ]  #purpose to use two extra white spaces just to handle indexing in a proper way
+
+    if num == 0:
+        return 'zero'
+
     
+    billions = num // 1000000000
+    millions = (num // 1000000) % 1000
+    thousands = (num // 1000) % 1000
+    hundreds = (num // 100) % 10
+    remainder = num % 100
 
-    tens
+    result = ''
 
+    if billions > 0:
+        result += number_to_words(billions) + ' billion '
 
+    if millions > 0:
+        result += number_to_words(millions) + ' million '   # number_to_words(millions) recursive fucntion
 
+    if thousands > 0:
+        result += number_to_words(thousands) + ' thousand '# number_to_words(millions) recrsive 
 
+    if hundreds > 0:
+        result += ones[hundreds] + ' hundred '
 
+    if remainder > 0:
+        if remainder < 20:
+            result += ones[remainder]
+        else:
+            result += tens[remainder // 10] + ' ' + ones[remainder % 10]
 
+    return result.strip() #eliminate any extra white space 
 
-num = int(input('Enter a number: '))
+number = int(input("Enter a number: "))
+words = number_to_words(number)   #function calling
+print(f"{number} in words: {words}")
 
-print(numeric_to_word(num))
