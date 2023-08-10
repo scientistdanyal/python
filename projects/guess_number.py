3ffb8ec14fe8329076f657d9b5b168ed9c69d832
yalgar0 <dk.yalgar@gmail.com>
2023-08-10 22:44:32 +0500

    guess number with python

---
+++ projects/guess_number.py
@@ -0,0 +1,29 @@
+from random import randint
+
+n1 = int(input('Enter upper limit: '))
+n2 = int(input('Enter lower limit: '))
+
+number = randint(n1,n2)
+
+chances = 0
+
+
+while chances < 8:
+
+    chances +=1 
+    guess = int(input('Enter Your Guess: '))
+    if number == guess:
+        print("Congratulations You Won. The number was {}".format(number))
+        break
+
+    elif guess < number:
+        print("you guessed a small number.")
+    elif guess > number:
+        print("You guessed is large number.")
+
+    if chances == 7:
+        print("You have run out of chances.\n")
+        print("The number was {}\n".format(number))
+        print("Better luck next time.")            
+        break
+    print('\n')
